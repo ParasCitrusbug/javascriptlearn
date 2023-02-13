e3bdd632278c718f53d2f38abe60e1565c6ee815
parascitrusbug <paras.citrusbug@gmail.com>
2023-02-13 10:27:05 +0530

    git commands

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+
+// let pre = document.querySelectorAll(".container")[1].innerHTML
+// firstContainer.addEventListener("mousedown",function(){
+//     document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
+//     console.log("Clicked on Container")
+
+// })
+
+// firstContainer.addEventListener("mouseup",function(){
+//     document.querySelectorAll('.container')[1].innerHTML = pre
+    
+
+// })
+
+log = ()=>{
+    console.log("log in create")
+}
+setTimeout(log, 2000);
